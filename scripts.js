cc0b93fd5afe0c0fb050528bdfba3627c394b522
liezhouzhong <zhongliezhou@gmail.com>
2024-04-30 15:03:11 +0800

    30April2024.3

--- scripts.js
+++ scripts.js
@@ -16,35 +16,6 @@ $(document).ready(function(){
     });
 });
 
-// Location pop-up window
-(function() {
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-
-    // After 10 seconds, open the modal if 24 hours have passed since the last display
-    setTimeout(function() {
-        var lastShown = localStorage.getItem('lastShown');
-        if (!lastShown || new Date() - new Date(lastShown) > 24*60*60*1000) {
-            modal.style.display = "block";
-            localStorage.setItem('lastShown', new Date());
-            }
-        }, 10000); // 10000 milliseconds = 10 seconds
-})();
 
 // collapse menu
 (function() {
@@ -110,6 +81,7 @@ $(document).ready(function(){
         });
     }
 })();
+
 
 
 
